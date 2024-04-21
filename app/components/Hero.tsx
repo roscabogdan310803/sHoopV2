9686bafc57b7ca4473c924adb5ef37d5ed6044c2
Rosca Nicolae Bogdan <160400506+roscabogdan310803@users.noreply.github.com>
2024-04-21 11:49:54 +0300

    Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -19,11 +19,10 @@ export default async function Hero() {
     <div className="mb-8 flex flex-wrap justify-between md:mb-16">
       <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
         <h1 className="mb-4 text-4xl font-bold text-porto1 sm:text-5xl md:mb-8 md:text-6xl">
-            Top Fashion for a top price!
+            The ultimate sneakers that everyone should own!
         </h1>
         <p className="max-w-md leading-relaxed text-porto2 xl:text-lg">
-            We sell only the most exclusive and high quality products for you.
-            We are the best so come and shop with us.
+            Discover the epitome of exclusivity and quality in our selection of sneakers. Elevate your style with the best – shop with us and experience
         </p>
       </div>
       <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
